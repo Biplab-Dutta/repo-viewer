d492bd2c5d54eacbaa17c17ecf843020ae829962
Biplab Dutta <biplabdutta860@gmail.com>
2022-04-11 11:23:27 +0545

    chore: initialize project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.repo_viewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:repo_viewer/app/app.dart';
+import 'package:repo_viewer/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}

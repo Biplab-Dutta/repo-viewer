d5d6829936474fc6f1e8b27913d3ef0743788126
Biplab Dutta <biplabdutta860@gmail.com>
2022-04-18 22:41:40 +0545

    feat: implement routing

---
+++ lib/routes/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:repo_viewer/features/auth/presentation/pages/authorization_page.dart';
+import 'package:repo_viewer/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:repo_viewer/features/splash/presentation/splash_page.dart';
+import 'package:repo_viewer/features/starred_repos/presentation/pages/starred_repos_page.dart';
+
+@MaterialAutoRouter(
+  routes: [
+    AutoRoute<bool>(page: SplashPage, initial: true),
+    AutoRoute<bool>(page: SignInPage, path: '/sign-in'),
+    AutoRoute<bool>(page: AuthorizationPage, path: '/auth'),
+    AutoRoute<bool>(page: StarredReposPage, path: '/starred')
+  ],
+  replaceInRouteName: 'Page,Route',
+)
+class $AppRouter {}

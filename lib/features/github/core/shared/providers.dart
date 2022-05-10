6749c91c6631124a88e3483b25d868a21c5105f6
Biplab Dutta <biplabdutta860@gmail.com>
2022-05-10 23:48:15 +0545

    feat(search): add provider

--- lib/features/github/core/shared/providers.dart
+++ lib/features/github/core/shared/providers.dart
@@ -1,8 +1,11 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:repo_viewer/features/core/shared/providers.dart';
 import 'package:repo_viewer/features/github/core/data/github_headers_cache.dart';
+import 'package:repo_viewer/features/github/repos/core/application/paginated_repos_notifier.dart';
+import 'package:repo_viewer/features/github/repos/searched_repos/application/searched_repos_notifier.dart';
+import 'package:repo_viewer/features/github/repos/searched_repos/data/data_sources/searched_repos_remote_data_source.dart';
+import 'package:repo_viewer/features/github/repos/searched_repos/data/repository/searched_repos_repository.dart';
 import 'package:repo_viewer/features/github/repos/starred_repos/application/starred_repos_notifier.dart';
-import 'package:repo_viewer/features/github/repos/starred_repos/application/starred_repos_state.dart';
 import 'package:repo_viewer/features/github/repos/starred_repos/data/data_sources/starred_repo_local_data_source.dart';
 import 'package:repo_viewer/features/github/repos/starred_repos/data/data_sources/starred_repo_remote_data_source.dart';
 import 'package:repo_viewer/features/github/repos/starred_repos/data/repository/starred_repos_repository.dart';
@@ -34,8 +37,28 @@ final starredReposRepositoryProvider = Provider(
 );
 
 final starredReposNotifierProvider =
-    StateNotifierProvider<StarredReposNotifier, StarredReposState>(
+    StateNotifierProvider<StarredReposNotifier, PaginatedReposState>(
   (ref) => StarredReposNotifier(
     ref.watch(starredReposRepositoryProvider),
+  ),
+);
+
+final searchedRepoRemoteDataSourceProvider = Provider(
+  (ref) => SearchedRepoRemoteDataSource(
+    ref.watch(dioProvider),
+    ref.watch(githubHeadersCacheProvider),
+  ),
+);
+
+final searchedReposRepositoryProvider = Provider(
+  (ref) => SearchedReposRepository(
+    ref.watch(searchedRepoRemoteDataSourceProvider),
+  ),
+);
+
+final searchedReposNotifierProvider =
+    StateNotifierProvider<SearchedReposNotifier, PaginatedReposState>(
+  (ref) => SearchedReposNotifier(
+    ref.watch(searchedReposRepositoryProvider),
   ),
 );
